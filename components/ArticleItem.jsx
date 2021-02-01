b818643c010ff84fe4a2abbf4e33c089b60aaecb
Rehim E. Firudunlu <FirudunluRE@kapitalho.az>
2021-02-01 20:10:16 +0400

    h3 -> h2

--- components/ArticleItem.jsx
+++ components/ArticleItem.jsx
@@ -5,7 +5,7 @@ const ArticleItem = ({ article }) => {
   return (
     <Link href="/article/[id]" as={`/article/${article.id}`}>
       <a className={articleStyle.card}>
-        <h3>{article.title} &rarr;</h3>
+        <h2>{article.title} &rarr;</h2>
         <p>{article.body}</p>
       </a>
     </Link>
